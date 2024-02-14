31d5427bf53895d3bd18e1659217e437a01a1f8f
Ask Hetland Berentsen <askberentsen@gmail.com>
2024-02-14 16:30:39 +0100

    Initialized VendingMachine

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task1
 {
@@ -7,6 +8,19 @@ namespace Task1
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            VendingMachine vendingMachine = new VendingMachine();
+            vendingMachine.List.Add("potato",2);
+            Console.WriteLine(vendingMachine.List["potato"]);
         }
+    }
+
+    internal class VendingMachine
+    {
+        public VendingMachine()
+        {
+            List = new Dictionary<string, int>();
+        }
+
+        public Dictionary<string, int> List { get; internal set; }
     }
 }
