4182545171d389f276908a5f50475737a976befa
Ask Hetland Berentsen <askberentsen@gmail.com>
2024-02-14 16:36:15 +0100

    renamed List property to Wares

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,15 +9,16 @@ namespace Task1
         {
             Console.WriteLine("Hello, World!");
             VendingMachine vendingMachine = new VendingMachine();
-            vendingMachine.List.Add("potato",2);
-            Console.WriteLine(vendingMachine.List["potato"]);
+            vendingMachine.Wares.Add("potato",2);
+            Console.WriteLine(vendingMachine.Wares["potato"]);
+            Console.WriteLine(vendingMachine.Credit);
         }
     }
 
     internal class VendingMachine
     {
-        public Dictionary<string, int> List { get; private set; } = new Dictionary<string, int>();
+        public Dictionary<string, int> Wares { get; private set; } = new Dictionary<string, int>();
         public int Credit { get; private set; } = 0;
-
+        
     }
 }
